771a0af5c24059a780beb58b78211cb0b016c1a7
lupin <lupin@lupinW>
2019-04-25 16:22:48 +0200

    Versió 0.1 del projecte prjava01

---
+++ src/prjava01/Prjava01.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01;
+import java.io.*;
+/**
+ *
+ * @author lupin
+ */
+public class Prjava01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+bw.write("<html>");
+bw.newLine(); 
+bw.write("  <head>");
+bw.newLine(); 
+bw.write("    <title>");
+bw.newLine();
+bw.write("      Nova p&agrave;gina  web");
+bw.newLine();
+bw.write("    </title>");
+bw.newLine(); 
+bw.write("  </head>");
+bw.newLine(); 
+bw.write("  <body>");
+bw.newLine();
+bw.write("    Nova p&agrave;gina web");
+bw.newLine(); 
+bw.write("  </body>");
+bw.newLine(); 
+bw.write("</html>");
+bw.newLine();
+bw.close();
+    }
+    }
+}
