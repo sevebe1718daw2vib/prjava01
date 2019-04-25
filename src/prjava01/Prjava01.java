ec1d9313039480adca81e9fafa56071ace824a85
lupin <lupin@lupinW>
2019-04-25 16:42:25 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -30,6 +30,8 @@ bw.newLine();
 bw.write("  </head>");
 bw.newLine(); 
 bw.write("  <body>");
+bw.write("    <h1>DAW2 m08uf4pr2</h1>");
+bw.newLine();
 bw.newLine();
 bw.write("    Nova p&agrave;gina web");
 bw.newLine(); 
